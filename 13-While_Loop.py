0e98ce9a5c6a1ee47ec8acd35ef9eee62a7509e9
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2023-11-26 10:38:26 +0530

    While Loop Done

---
+++ 13-While_Loop.py
@@ -0,0 +1,14 @@
+# i=0
+# while(i<3):
+#     print(i)
+#     i=i+1
+# i=3
+# while(i>0):
+#     print(i)
+#     i=i-1
+
+i=0
+while(i<=38):
+    i=int(input('enter a number:'))
+    print(i)
+print("done with the loop")
