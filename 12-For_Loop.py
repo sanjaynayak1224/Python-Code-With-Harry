fe3d7885f504e1e1af8b0088e945cff32f8fa9ce
sanjaynayak1224 <sanbiomedsanjay@gmail.com>
2023-11-26 10:20:52 +0530

    For Loop Done

---
+++ 12-For_Loop.py
@@ -0,0 +1,19 @@
+for i in range(10):
+    print(i,end='')
+print()
+
+#strings
+name='sanjay'
+for i in name:
+    print(i,end=' ')
+print()
+
+#lists
+color=['red','green','blue']
+for colors in color:
+    print(colors)
+    for i in colors:
+        print(i)
+
+for i in range(0,12,2):
+    print(i,end=' ')
